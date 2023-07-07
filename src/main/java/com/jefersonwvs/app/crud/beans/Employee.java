b7be3e16be6ee8996abc30b75a77cb6f5408a393
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-07-06 20:48:38 -0400

    Add beans

---
+++ src/main/java/com/jefersonwvs/app/crud/beans/Employee.java
@@ -0,0 +1,84 @@
+package com.jefersonwvs.app.crud.beans;
+
+public class Employee {
+
+  private int id;
+  private String name;
+  private String email;
+  private String phone;
+  private String address;
+
+  private Employee() {}
+
+  public Employee(String name, String email, String phone, String address) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.address = address;
+  }
+
+  public Employee(int id, String name, String email, String phone, String address) {
+    this(name, email, phone, address);
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{"
+        + "id="
+        + id
+        + ", name='"
+        + name
+        + '\''
+        + ", email='"
+        + email
+        + '\''
+        + ", phone='"
+        + phone
+        + '\''
+        + ", address='"
+        + address
+        + '\''
+        + '}';
+  }
+}

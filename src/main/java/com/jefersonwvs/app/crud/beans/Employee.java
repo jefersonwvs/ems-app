7d0a7b872c4c778f3d56efc5c98bdd596aa9ef89
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-07-07 23:13:18 -0400

    Add insert new employee on controller layer

--- src/main/java/com/jefersonwvs/app/crud/beans/Employee.java
+++ src/main/java/com/jefersonwvs/app/crud/beans/Employee.java
@@ -2,7 +2,7 @@ package com.jefersonwvs.app.crud.beans;
 
 public class Employee {
 
-  private int id;
+  private Integer id;
   private String name;
   private String email;
   private String phone;
@@ -22,11 +22,11 @@ public class Employee {
     this.id = id;
   }
 
-  public int getId() {
+  public Integer getId() {
     return id;
   }
 
-  public void setId(int id) {
+  public void setId(Integer id) {
     this.id = id;
   }
 
--- src/main/java/com/jefersonwvs/app/crud/controller/EmployeeController.java
+++ src/main/java/com/jefersonwvs/app/crud/controller/EmployeeController.java
@@ -1,16 +1,18 @@
 package com.jefersonwvs.app.crud.controller;
 
+import com.jefersonwvs.app.crud.beans.Employee;
 import com.jefersonwvs.app.crud.dao.EmployeeDAO;
 import com.jefersonwvs.app.crud.dao.EmployeeDAOImpl;
-
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("/controller")
+@WebServlet("/")
 public class EmployeeController extends HttpServlet {
 
   private EmployeeDAO employeeDAO = null;
@@ -36,27 +38,40 @@ public class EmployeeController extends HttpServlet {
     System.out.println("doPost action ==> " + action);
 
     switch (action) {
-      case "add":
+      case "/add":
         addNewEmployee(request, response);
         break;
-      case "get":
+      case "/get":
         getEmployee(request, response);
         break;
-      case "list":
+      case "/list":
         getAllEmployees(request, response);
         break;
-      case "update":
+      case "/update":
         updateEmployee(request, response);
         break;
-      case "delete":
+      case "/delete":
         deleteEmployee(request, response);
         break;
       default:
-        addNewEmployee(request, response);
+        getAllEmployees(request, response);
     }
   }
 
-  private void addNewEmployee(HttpServletRequest request, HttpServletResponse response) {}
+  private void addNewEmployee(HttpServletRequest request, HttpServletResponse response) {
+    Employee newEmployee = EmployeeController.getEmployeeFromRequest(request);
+    System.out.println("addNewEmployee method invoked, employee details ==> " + newEmployee);
+    boolean result = employeeDAO.addEmployee(newEmployee);
+    System.out.println(
+        "Employee \"" + newEmployee.getName() + "\" was " + (!result ? "not " : "") + "added");
+
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/employeesView.jsp");
+    try {
+      dispatcher.forward(request, response);
+    } catch (ServletException | IOException e) {
+      e.printStackTrace();
+    }
+  }
 
   private void getEmployee(HttpServletRequest request, HttpServletResponse response) {}
 
@@ -65,4 +80,18 @@ public class EmployeeController extends HttpServlet {
   private void updateEmployee(HttpServletRequest request, HttpServletResponse response) {}
 
   private void deleteEmployee(HttpServletRequest request, HttpServletResponse response) {}
+
+  private static Employee getEmployeeFromRequest(HttpServletRequest request) {
+    Employee employee = new Employee();
+    Optional<Integer> idOptional =
+        Optional.ofNullable(request.getParameter("id")).map(Integer::parseInt).or(Optional::empty);
+
+    employee.setId(idOptional.orElse(null));
+    employee.setName(request.getParameter("name"));
+    employee.setEmail(request.getParameter("email"));
+    employee.setPhone(request.getParameter("phone"));
+    employee.setAddress(request.getParameter("address"));
+
+    return employee;
+  }
 }
